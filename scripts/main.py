9d4c5e6958feb95c89f2f394681281c357a44d69
Jack MB <hi@jackmurraybrown.com>
2025-02-24 13:16:42 +0100

    move main.py back into scripts for deployment testing

--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,5 @@
-from scripts.audio_utils import process_audio_files, download_file
-from scripts.upload_utils import get_drive_service
+from audio_utils import process_audio_files, download_file
+from upload_utils import get_drive_service
 from pydub import AudioSegment
 import os
 
